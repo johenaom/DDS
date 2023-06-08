6ebf3fd474447bb67dbf6c18d1e45ed4f8e52f3f
Jose Oscar Henao Monje <36483775+johenaom@users.noreply.github.com>
2023-06-08 16:06:11 -0500

    Update CAP. 1. INTRODUCCIÓN - DSS.R

--- CAP. 1. INTRODUCCIÓN - DSS.R
+++ CAP. 1. INTRODUCCIÓN - DSS.R
@@ -55,7 +55,21 @@ View(star) # abre un nueva tabla que contiene la base de datos STAR
 head(star) # muestra las primeras seis filas en la consola
 head(star, n=3) # Nota: se puede especificar que muestre las primeras 3 filas
 
+#5. Cantidad de observaciones
+dim(star) # muestra que la base de datos tiene 1274 filas y 4 columnas
 
+#6. Acceder a variables en un dataframe
+star$reading # Este nos muestra la totalidad de observaciones contenidas en la varibale reading
+
+#7. Promedio
+mean(star$reading) # nos muestra que el promedio es 628.803
+
+#8. Promedio de variable binaria
+mean(star$graduated) # 87% de los estudiantes en promedio se graduaron.
+
+##################################
+#### FIN DE PRIMER CAPÍTULO ######
+##################################
 
 
 
